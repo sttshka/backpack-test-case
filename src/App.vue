714ad95950d1d46abb198d92e7501b1457e526c9
Andrew Peshko <apeshko@metaprime.ru>
2023-08-20 13:44:06 +0300

    Init commit + added backpack base

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { RouterView } from "vue-router";
+import Backpack from "./components/Backpack.vue";
+</script>
+
+<template>
+  <Backpack />
+  <RouterView />
+</template>
---
+++ src/components/Backpack.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="backpack">
+    <div class="backpack__header">
+      <div>Backpack</div>
+      <div>Nexus</div>
+      <div>Close</div>
+    </div>
+    <div class="backpack__content">
+      <div class="backpack__filter"></div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.backpack {
+  width: 562px;
+  height: 864px;
+  justify-content: center;
+  align-items: center;
+  background-color: #454545;
+}
+</style>
